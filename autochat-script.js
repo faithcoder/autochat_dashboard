03edd446bae02044d5c7f23b7912f49a6f41b80e
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-06-14 14:40:27 +0600

    file upload and remove button action added by jquery

--- autochat-script.js
+++ autochat-script.js
@@ -17,4 +17,32 @@ $(document).ready(function() {
             $('#bubbleIconArea').show();
         }
     });
+
+
+    // Default image source
+    var defaultImageSrc = 'img/avatar.png';
+
+    // Handle file input change event
+    $('#icon-file').change(function(event) {
+        var input = event.target;
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function(e) {
+                $('.upload-image').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+
+    // Handle remove button click event
+    $('.remove-button').click(function() {
+        $('#icon-file').val('');
+        $('.upload-image').attr('src', defaultImageSrc);
+    });
+
 });
+
+
